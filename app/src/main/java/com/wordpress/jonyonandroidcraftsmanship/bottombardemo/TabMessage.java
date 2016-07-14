b5348f712e5f92fde8165bc81c9495c265ce9ecc
MdManjurulHoqueJony <kucse03jony@gmail.com>
2016-07-14 17:59:16 +0600

    Initial commit

---
+++ app/src/main/java/com/wordpress/jonyonandroidcraftsmanship/bottombardemo/TabMessage.java
@@ -0,0 +1,31 @@
+package com.wordpress.jonyonandroidcraftsmanship.bottombardemo;
+
+public class TabMessage {
+    public static String get(int menuItemId, boolean isReselection) {
+        String message = "Content for ";
+
+        switch (menuItemId) {
+            case R.id.bb_menu_recents:
+                message += "recents";
+                break;
+            case R.id.bb_menu_favorites:
+                message += "favorites";
+                break;
+            case R.id.bb_menu_nearby:
+                message += "nearby";
+                break;
+            case R.id.bb_menu_friends:
+                message += "friends";
+                break;
+            case R.id.bb_menu_food:
+                message += "food";
+                break;
+        }
+
+        if (isReselection) {
+            message += " WAS RESELECTED! YAY!";
+        }
+
+        return message;
+    }
+}
